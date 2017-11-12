2f08a6a5107a6d92192934fc6ed8780c66a3c47d
Atsushi Kimura <sudachi808@gmail.com>
2017-11-12 23:04:20 +0900

    add CircleView for iOS

---
+++ Sample/CircleView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public class CircleView : ContentView
+    {
+    }
+}
+
---
+++ iOS/CircleViewRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using CoreGraphics;
+using UIKit;
+
+using Sample;
+using Sample.iOS;
+
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
+
+namespace Sample.iOS
+{
+    public class CircleViewRenderer : ViewRenderer
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            
+            var context = UIGraphics.GetCurrentContext();
+            
+            context.SetFillColor(Color.Aqua.ToCGColor());
+            context.FillEllipseInRect(rect);
+        }
+    }
+}
+
